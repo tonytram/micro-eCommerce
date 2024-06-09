41ebccff0a9ad887e5c4883e4c30cd9408e943fe
Tony Tram <tri.tram_ext@b4value.net>
2024-06-10 06:43:07 +0700

    Developing basket

---
+++ src/Services/Basket/Basket.API/Data/RedisBasketRepository.cs
@@ -0,0 +1,28 @@
+﻿using Basket.API.Models;
+using StackExchange.Redis;
+using System.Runtime.CompilerServices;
+
+namespace Basket.API.Data
+{
+    public class RedisBasketRepository(ILogger<RedisBasketRepository> logger, IConnectionMultiplexer redis) : IBasketRepository
+    {
+        private readonly IDatabase _database = redis.GetDatabase();
+
+        private static RedisKey BasketKeyPrefix = "/basket/"u8.ToArray();
+
+        public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
